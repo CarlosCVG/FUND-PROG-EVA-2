452c2541756c3c06f26bd6eb8e901a6246f85be8
CarlosCVG <143562985+CarlosCVG@users.noreply.github.com>
2023-10-18 17:45:46 -0600

    Add files via upload

---
+++ EVA2_11_CADENAS/src/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_11_cadenas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_11_CADENAS {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      String cadena= "Hola";
+      
+      //Para comparar cadenas se usa la funcion .equals("Args"); despues del nombre de la variable: cadea.equals("Hola");
+      
+      //if (cadena == "Hola")
+      if (cadena.equals("Hola"))
+         System.out.println("Las cadenas son iguales");
+      else
+         System.out.println("Las cadenas son diferentes");
+   }
+   
+}
