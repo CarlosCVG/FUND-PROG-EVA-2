b779c0f132eabda32b5a90061904fae8450aa5f1
CarlosCVG <143562985+CarlosCVG@users.noreply.github.com>
2023-11-07 17:54:05 -0600

    Add files via upload

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.println("Introduce el primer número:");
+        num1 = teclado.nextInt();
+        System.out.println("Introduce el segundo número:");
+        num2 = teclado.nextInt();
+
+        if ((num1 >= 0) && (num2 >= 0)) {
+            if (num2 > num1) {
+                for (int i = num1; i <= num2; i++) {
+                    if ((i%2)==0)
+                    System.out.print(i + "-");
+                }
+            } else if (num1 > num2) {
+                for (int i = num2; i <= num1; i++) {
+                    if ((i%2)==0)
+                    System.out.print(i + "-");
+                }
+            } else {
+                System.out.println("Los números son iguales");
+            } 
+        } else {
+            System.out.println("Alguno de los 2 números no son positivos");
+        }
+        System.out.println("");
+    }
+}
